85cb05237a269e1677a21fb4b81b005aec76cfa7
Gemino <71484444@idexperujapon.edu.pe>
2025-06-20 18:13:04 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, suma;
+            suma = 0;
+            Console.WriteLine("Ingrese un número:");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            suma = suma + n1;
+            while (n1!=0)
+            {
+                Console.WriteLine("Ingrese un número:");
+                n1= Convert.ToInt32(Console.ReadLine());
+                suma = suma + n1;
+            }
+            Console.WriteLine("La suma de todos los valores es: "+suma);
+        }
+    }
+}
