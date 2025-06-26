eb3cb6fc61cbfaf83c7790e3d9b8e5dc6b6f30d1
Gemino <71484444@idexperujapon.edu.pe>
2025-06-26 17:36:47 -0500

    finalizado

--- Ejercicio_10/Program.cs
+++ Ejercicio_10/Program.cs
@@ -10,17 +10,21 @@ namespace Ejercicio_10
     {
         static void Main(string[] args)
         {
+            //definimos variables
             int n1, suma;
+            //variable suma en 0
             suma = 0;
+            //solicitamos el ´primer número para que no haya error más abajo
             Console.WriteLine("Ingrese un número:");
             n1 = Convert.ToInt32(Console.ReadLine());
-            suma = suma + n1;
+            //while repite la acción hasta que el número ingresado sea 0
             while (n1!=0)
             {
+                suma = suma + n1;
                 Console.WriteLine("Ingrese un número:");
                 n1= Convert.ToInt32(Console.ReadLine());
-                suma = suma + n1;
             }
+            //muestra la suma de todos los valores
             Console.WriteLine("La suma de todos los valores es: "+suma);
         }
     }
